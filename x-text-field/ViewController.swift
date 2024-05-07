148e9c2a11dd15c7000409b9e78601872cfcbb84
Вячеслав Ворожейкин <Vyacheslav.Vorozheykin@leroymerlin.ru>
2024-05-07 11:21:25 +0700

    Initial Commit

---
+++ x-text-field/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  x-text-field
+//
+//  Created by Слава on 07.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
